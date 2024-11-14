8c807359e4cfd390f4ac4b74d22ce0d5445cef68
Nilesh-567 <137327289+Nilesh-567@users.noreply.github.com>
2024-11-14 12:14:59 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     reactStrictMode: true,
     env: {
-      DATABASE_URL: process.env.DATABASE_URL || "postgresql://myuser:uSfub3pbJM4MGK58DQkcW0oqG73hxG1Y@dpg-csq3ic9u0jms73fmoeig-a.oregon-postgres.render.com/mydb_hswv", // Make sure the environment variable is accessible
+      DATABASE_URL: process.env.DATABASE_URL || "postgresql://myuser:uSfub3pbJM4MGK58DQkcW0oqG73hxG1Y@dpg-csq3ic9u0jms73fmoeig-a/mydb_hswv", // Make sure the environment variable is accessible
     },
   };
   
